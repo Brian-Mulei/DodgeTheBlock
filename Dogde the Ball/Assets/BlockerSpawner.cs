d0e247dcaf581d606ad8f65a0bf8ac214239f6f0
brian-mulei <brianmulei0@gmail.com>
2021-03-29 01:06:34 +0300

    First commit
    First commit

---
+++ Dogde the Ball/Assets/BlockerSpawner.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockerSpawner : MonoBehaviour
+{ 
+     public Transform[] spawnPoints;
+public GameObject block;
+public float timeBetweenWaves = 1f;
+
+    public float spawnTime = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Time.time >= spawnTime)
+        {
+            SpawnBlocks();
+            spawnTime = Time.time + timeBetweenWaves;
+        }
+    }
+    void SpawnBlocks()
+    {
+
+        int randomIndex = Random.Range(0, spawnPoints.Length);
+
+        for (int i = 0; i < spawnPoints.Length; i++)
+        {
+
+            if (randomIndex != i)
+            {
+                Instantiate(block, spawnPoints[i].position, Quaternion.identity);
+            }
+        }
+
+
+    }
+}
+    
---
+++ Dogde the Ball/Assets/player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public float speed = 15f;
+    private Rigidbody2D rb;
+    public float mapWidth = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal") * Time.fixedDeltaTime * speed;
+
+
+        Vector2 newPosition = rb.position + Vector2.right * x;
+        newPosition.x = Mathf.Clamp(newPosition.x, -mapWidth, mapWidth);
+        rb.MovePosition(newPosition);
+    }
+    void OnCollisionEnter2D()
+    {
+
+        FindObjectOfType<GameManager>().EndGame();
+     }
+
+}
